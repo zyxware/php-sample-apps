7c3df9afe945129e928b7d5024a6d2a3a1ca7003
Clive Verghese <cliveverghese@gmail.com>
2012-12-14 09:18:45 +0530

    Converted SQL statements to prepared statements

--- getdetails.php
+++ getdetails.php
@@ -6,8 +6,10 @@ require_once './config/config.php';
 $dbh = new PDO("mysql:host=localhost;dbname=" . $DB_NAME, $DB_USER, $DB_PASSWORD);
 $sql = "SELECT `item_name`,`price`"
 . " FROM `stock`"
-. " WHERE `code` = '" . $_REQUEST['code'] . "'";
-$ref = $dbh->query($sql);
+. " WHERE `code` = :stock_code";
+$ref = $dbh->prepare($sql);
+$ref->bindParam(':stock_code',$_REQUEST['code']);
+$ref->execute();
 $row = $ref->fetch();
 $return['item'] = $row['item_name'];
 $return['price'] = $row['price'];
